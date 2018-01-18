d75bfc59e1688b7480116794caaf34d5dd4ca373
Derek J Wang <d95wang@uw.edu>
2018-01-18 15:33:23 -0800

    add docker file to build

--- servers/gateway/scripts/build.sh
+++ servers/gateway/scripts/build.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
+
+# Move to the main folder
+cd ../
+
 set -e
 echo "Gateway Building..."
 # This might need to change based on what Operating System you use
@@ -8,10 +12,10 @@ CGO_ENABLED=0 go build -a
 # to your own docker hub in case yow want to build at the same time
 
 # Build gateway
-docker build -t d95wang/gateway .
+docker build -t d95wang/halp-gateway .
 
 # Push the gateway to dockerhub
-docker push d95wang/gateway
+docker push d95wang/halp-gateway
 
 # Clean something?
 go clean
