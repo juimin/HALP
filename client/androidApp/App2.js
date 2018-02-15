c949f51e804345f008906e2a91b6dcc004e5a2a8
AG <agilbe@uw.edu>
2018-02-15 01:16:07 -0800

    testing

---
+++ client/androidApp/App2.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Provider } from 'react-redux'
+import createStore from './redux'
+/*
+* Both of the following files work for react-navigation
+* Routes will always be added and supported by modifying
+* the AppNavigation file.  Special redux actions/reducers
+* will be handled in Redux Navigation
+*   // use this to use react-navigation no redux
+*   import AppNavigation from './Navigation/AppNavigation'
+*
+*   // use this to use react-navigation with redux
+*   import ReduxNavigation from './Navigation/ReduxNavigation'
+*/
+
+// We're going to use navigation with redux
+import ReduxNavigation from './nav/ReduxNavigation'
+
+// create our store
+const store = createStore()
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <ReduxNavigation />
+        </View>
+      </Provider>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+})
