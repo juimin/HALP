cdc3ae0bacad358a5388891e90ed9f523fb701ad
AG <agilbe@uw.edu>
2018-02-27 19:08:40 -0800

    added navigation + basic drawing

---
+++ client/androidApp/src/HomeNav.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button, StyleSheet, View, Text } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import HomeScreen from './HomeScreen';
+import SignupScreen from './SignupScreen';
+import LoginScreen from './LoginScreen';
+import CanvasTest from './CanvasTest';
+
+export default class HomeNav extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Home',
+    tabBarIcon: () => (<Icon size={24} color="white" name="home" />)
+  }
+ 
+  render() {
+    return <RootStack screenProps={{loggedin: false}}/>;
+  }
+}
+
+//react-navigation
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: { title: 'Home' },
+    },
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: { title: 'Log In' },
+    },
+    Signup: {
+    	screen: SignupScreen,
+    	navigationOptions: { title: 'Sign Up' },
+    },
+    Canvas: {
+      screen: CanvasTest,
+      navigationOptions: {title: 'Edit Image'},
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode: 'screen',
+
+  },
+);
