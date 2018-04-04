0bc47727b006da199b6b8c96c3ea3646d9552cb3
JuiMin <d95wang@gmail.com>
2018-04-03 22:57:40 -0700

    add emulator stuff

--- clients/flutter_drawing/lib/main.dart
+++ clients/flutter_drawing/lib/main.dart
@@ -12,9 +12,31 @@ class DrawingWidget extends StatelessWidget {
           title: new Text('Drawing'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          child: new CustomPaint(
+            painter: new Sky(),
+            child: new Center(
+              child: new Text(
+                'Once upon a time...',
+                style: const TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w900,
+                  color: const Color(0xFFFFFFFF),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+class NavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new BottomNavigationBar(
+      
+    );
+  }
+}
+
