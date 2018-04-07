9794cb8a95df33852b48e25e5d6fed5fcb567e9a
dlee20013 <dlee20013@gmail.com>
2018-04-06 18:59:30 -0700

    Board finalzed object

--- servers/gateway/models/boards/boards.go
+++ servers/gateway/models/boards/boards.go
@@ -7,8 +7,33 @@ type Board struct {
 	ID          bson.ObjectId `json:"id" bson:"_id"`
 	Title       string        `json:"title"`
 	Description string        `json:"description"`
-	Icon        string        `json:"icon"`
 	Image       string        `json:"image"`
-	Subcribers  int           `json:"subscribers"`
+	Subscribers int           `json:"subscribers"`
 	Posts       int           `json:"posts"`
+}
+
+// ChangeSubscriberCount takes in a boolean value to represent a change in subscribers
+func (b *Board) ChangeSubscriberCount(update bool) {
+	if b.Subscribers >= 0 {
+		if update {
+			b.Subscribers++
+		} else {
+			b.Subscribers--
+		}
+	} else {
+		b.Subscribers = 0
+	}
+}
+
+// ChangePostCount takes in a boolean value to represent a change in posts
+func (b *Board) ChangePostCount(update bool) {
+	if b.Posts >= 0 {
+		if update {
+			b.Posts++
+		} else {
+			b.Posts--
+		}
+	} else {
+		b.Posts = 0
+	}
 }
