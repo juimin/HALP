5e95e69ad58e93449bea84350586afc5a348ceba
AG <agilbe@uw.edu>
2018-04-17 15:12:13 -0700

    posts.mongo_store properly implements store for main.go

--- servers/gateway/models/posts/store.go
+++ servers/gateway/models/posts/store.go
@@ -18,14 +18,14 @@ type Store interface {
 	// it into the database, and returns it
 	Insert(newPost *NewPost) (*Post, error)
 
-	//GetById returns the Post with the given ID
-	GetById(id bson.ObjectId) (*Post, error)
+	//GetByID returns the Post with the given ID
+	GetByID(id bson.ObjectId) (*Post, error)
 
-	//GetByAuthorId returns all Posts by a given author by ID
-	GetByAuthorId(id bson.ObjectId) (*[]Post, error)
+	//GetByAuthorID returns all Posts by a given author by ID
+	GetByAuthorID(id bson.ObjectId) (*[]Post, error)
 
-	//GetByBoardId returns all Posts for a given board by ID
-	GetByBoardId(id bson.ObjectId) (*[]Post, error)
+	//GetByBoardID returns all Posts for a given board by ID
+	GetByBoardID(id bson.ObjectId) (*[]Post, error)
 
 	//Delete deletes the post with the given ID
 	Delete(id bson.ObjectId) error
