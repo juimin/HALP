c2a0ab7c0a7c782f3bafc6b7c8cd94ee5a008c38
dlee20013 <dlee20013@gmail.com>
2018-04-24 16:30:07 -0700

    boards handler mongo_store update

---
+++ servers/gateway/handlers/board_test.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestBoardsAllHandler(t *testing.T) {
+	for _, c := range cases {
+		req, err := http.NewRequest("GET", "/")
+	}
+}
+
+func TestSingleBoardHandler(t *testing.T) {
+
+}
+
+func TestUpdatePostHandler(t *testing.T) {
+
+}
+
+func TestUpdateSubscriberHandler(t *testing.T) {
+
+}
