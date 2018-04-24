4247a15aa5b864b4a7de4279c9cd9686edf21ad2
dlee20013 <dlee20013@gmail.com>
2018-04-24 16:45:15 -0700

    empty func

--- servers/gateway/handlers/board_test.go
+++ servers/gateway/handlers/board_test.go
@@ -1,14 +1,11 @@
 package handlers
 
 import (
-	"net/http"
 	"testing"
 )
 
 func TestBoardsAllHandler(t *testing.T) {
-	for _, c := range cases {
-		req, err := http.NewRequest("GET", "/")
-	}
+
 }
 
 func TestSingleBoardHandler(t *testing.T) {
