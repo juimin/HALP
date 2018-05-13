a13ba29bdddf59909f38d5627036a371a0594ce8
JuiMin <d95wang@gmail.com>
2018-05-13 10:18:43 -0700

    get rid of that thing we don't need

--- clients/HalpTest/src/Components/Settings/Settings.js
+++ clients/HalpTest/src/Components/Settings/Settings.js
@@ -7,18 +7,9 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 import Theme from '../../Styles/Theme'
 
 export default class Settings extends React.Component {
-   // Set the icon color when selected ??
-   // What is this actually for?
-   static navigationOptions = {
-      tabBarIcon: (tintColor) => (
-         <Icon size={Theme.tabBar.iconSize} name="settings" style={{color:tintColor}}/>
-      )
-   }
- 
    render() {
       return (
          <Text>this is the settings page</Text>
       )
    }
 }
-
