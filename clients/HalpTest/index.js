f0c3b3cad6e27f3702dee80bb4a5577ccf55e9e5
JuiMin <d95wang@gmail.com>
2018-05-12 19:21:35 -0700

    we can't do this yet because it's too important

--- clients/HalpTest/index.js
+++ clients/HalpTest/index.js
@@ -1,3 +1,7 @@
+// Import the Application bundle in the App component
+// as well as react native app registry to run
 import { AppRegistry } from 'react-native';
 import App from './App';
+
+// Register the HALP test component
 AppRegistry.registerComponent('HalpTest', () => App);
