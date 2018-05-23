f39fd6c194ce3a37fc6b7830b197ce7c45f2934c
dlee20013 <dlee20013@gmail.com>
2018-05-22 20:14:19 -0700

    just pushing stuff so I don't lose schtuff

--- clients/HalpTest/src/Redux/ActionTypes.js
+++ clients/HalpTest/src/Redux/ActionTypes.js
@@ -3,3 +3,8 @@
 export const AUTH = "AUTH";
 export const DOWNLOAD = "DOWNLOAD";
 export const LOGIN = "LOGIN";
+
+// Boards action types defintions 
+export const FETCH_BOARDS_BEGIN = "FETCH_BOARDS_BEGIN";
+export const FETCH_BOARDS_SUCCESS = "FETCH_BOARDS_SUCCESS";
+export const FETCH_BOARDS_FAILIURE = "FETCH_BOARDS_FAILIURE";
