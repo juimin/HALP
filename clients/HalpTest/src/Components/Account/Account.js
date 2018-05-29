12a1edd6e38953849d144188f181026e3962ebef
JuiMin <d95wang@gmail.com>
2018-05-28 20:58:30 -0700

    account stuff

--- clients/HalpTest/src/Components/Account/Account.js
+++ clients/HalpTest/src/Components/Account/Account.js
@@ -2,52 +2,110 @@
 
 // Import react components
 import React, { Component } from 'react';
-import { Button, ScrollView, View, Text } from 'react-native';
+import { ScrollView, View} from 'react-native';
 import { TabNavigator } from 'react-navigation';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import {
+	Container,
+	Right,
+	Body,
+	Left,
+	Title,
+	Subtitle,
+	Header,
+	Button,
+	Icon,
+	Thumbnail,
+	FooterTab,
+	Content,
+	Text,
+	ActionSheet,
+} from 'native-base';
 
 // Import React Native Elements
 import { ButtonGroup } from 'react-native-elements';
 
-// Import the header
-import AccountHeader from './AccountHeader';
-
 // Import the styles and themes
 import Styles from '../../Styles/Styles';
 import Theme from '../../Styles/Theme';
+// Import redux
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class Account extends Component {
+const mapStateToProps = (state) => {
+	return {
+		authToken: state.AuthReducer.authToken,
+		password: state.AuthReducer.password,
+		user: state.AuthReducer.user
+	}
+}
 
-   constructor()  {
-      super()
+class Account extends Component {
+
+   constructor(props) {
+      super(props)
       this.state = {
-         selectedIndex: 0
+			menu: {
+				selectedIndex: -1
+			}
       }
-      this.switchSelection = this.switchSelection.bind(this)
-   }
-
-   switchSelection(selectedIndex) {
-      // Set the index so that the component knows where we are
-      this.setState({selectedIndex})
-   }
+	}
 
    render() {
-      const { selectedIndex } = this.state  
+		if (this.props.user == null) {
+			return(
+				<Container style={Styles.home}>
+					<Button rounded style={Styles.button} 
+						onPress={() => this.props.navigation.navigate('Login')}
+					>
+						<Text>Log In</Text>
+					</Button>
+				</Container>
+			
+			);
+		}
+
       return (
-         <View>
-            <AccountHeader />
-            <ButtonGroup
-               onPress={this.switchSelection}
-               selectedIndex={selectedIndex}
-               buttons={['Saved', 'Posts', 'Comments', 'History']}
-               containerStyle={Styles.accountNavButtons}
-            />
-            <ScrollView>
-               {
-                  // Apend cards here
-                }
-            </ScrollView>
-         </View>
+        <Container>
+			  	<Header style={Styles.accountHeader}>	
+					<Right>
+						<Button transparent>
+							<Icon name='create' />
+						</Button>
+						<Button transparent
+							onPress={() =>
+							ActionSheet.show(
+								{
+									options: ['Log Out' , 'Cancel'],
+									cancelButtonIndex: 1,
+									title: "Options"
+								},
+								buttonIndex => {
+									console.log(this.state)
+									this.setState({
+										menu: {
+											selectedIndex: buttonIndex
+										}
+									});
+									console.log(this.state)
+								}
+							)}
+						>
+							<Icon name='more' />
+						</Button>
+					</Right>
+				</Header>
+				<Header span style={Styles.accountHeader}>
+					<Left>
+					  <Thumbnail style={Styles.accountThumbnail} large source={{uri: "https://facebook.github.io/react-native/docs/assets/favicon.png"}} />
+					</Left>
+					<Body style={Styles.accountTitle}>
+						<Title>Name</Title>
+						<Subtitle>Filler</Subtitle>
+					</Body>
+				</Header>
+        </Container>
       )
   }
 }
+
+export default connect(mapStateToProps)(Account)
